1f5debab791c09ae7a7ed589cfe6d2858a9fe9ca
Abed Arrahman Alhennawi <you@example.com>
2025-03-07 23:36:02 +0300

    Your commit message here

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod document;
+mod inverted_index;
+mod utils;
+
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    let mut index = inverted_index::InvertedIndex::new();
+    for doc in document::Document::from_vec(&utils::read_dir("songs").unwrap()) {
+        index.add_document(&doc)
+    }
+    index.save("inverted_index.json").unwrap();
+
+    let loaded_index = inverted_index::InvertedIndex::load("inverted_index.json")
+        .unwrap();
+
+    for res in loaded_index.search("love") {
+        println!("Song: {}\nScore: {}\n", res.0, res.1);
+    }
+    println!("Time it took: {:?}", start.elapsed())
+}
---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+use std::fs;
+use std::io;
+
+pub fn read_dir(path: &str) -> io::Result<Vec<String>> {
+    let mut files = Vec::new();
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let file_name = entry.file_name().into_string().unwrap_or_else(|_| "<Invalid UTF-8>".to_string());
+        files.push(format!("{}/{}", path, file_name));
+    }
+    Ok(files)
+}
